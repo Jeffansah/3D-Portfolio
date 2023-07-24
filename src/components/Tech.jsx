01450806a058aa731bfc8fa40ad33f3b95d72cc9
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-07-24 18:01:17 +0000

    3d ball Mesh implemented

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,7 +3,15 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  return <div>Tech</div>;
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech, index) => (
+        <div className="w-28 h-28" key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SectionWrapper(Tech, "");
