fcd44afc93d948d135950a6667813a6e079b1bcd
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-07-28 01:33:04 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
           </h1>
           <p
             className={
-              " font-light lg:text-[25px] sm:text-[22px] xs:text-[18px] text-[16px] lg:leading-[40px] mt-5 lg:mt-5 text-gray-100"
+              " font-light lg:text-[25px] sm:text-[22px] xs:text-[18px] text-[16px] lg:leading-[40px] sm:mt-5 lg:mt-5 text-gray-100"
             }
           >
             I am a Fullstack Web Developer. <br /> I build and design beautiful,
@@ -36,7 +36,7 @@ const Hero = () => {
             of the internet to you
           </p>
           <a href="#contact">
-            <button className="mt-10 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-purple-600 group-hover:from-green-500 group-hover:to-purple-700 hover:text-white dark:text-white ">
+            <button className="mt-5 sm:mt-10 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-purple-600 group-hover:from-green-500 group-hover:to-purple-700 hover:text-white dark:text-white ">
               <span className="relative px-5 py-2 sm:px-8 sm:py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 sm:text-lg text-md">
                 Get in touch
               </span>
@@ -58,7 +58,7 @@ const Hero = () => {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="sm:w-14 sm:h-14 w-12 h-12"
+                  className="sm:w-14 sm:h-14 w-10 h-10"
                 >
                   <path
                     strokeLinecap="round"
@@ -77,7 +77,7 @@ const Hero = () => {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="sm:w-14 sm:h-14 w-12 h-12"
+                  className="sm:w-14 sm:h-14 w-10 h-10"
                 >
                   <path
                     strokeLinecap="round"
