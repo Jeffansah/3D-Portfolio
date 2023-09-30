33cb85e5250de74f600819badfb3fea327d66c1a
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-30 06:16:40 +0000

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,7 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((tech, index) => (
-        <div className="w-28 h-28" key={tech.name}>
+        <div className="w-28 h-28 cursor-pointer" key={tech.name}>
           <BallCanvas icon={tech.icon} />
         </div>
       ))}
